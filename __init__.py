f97d2241f2c788492053a99a32092c00fd2fa6f0
Sylvia van Os <sylvia@hackerchick.me>
2018-06-17 17:06:14 +0200

    Syntax differences

--- __init__.py
+++ __init__.py
@@ -159,7 +159,7 @@ class Module(ModuleBase):
         sanitizedCommandList = [quote(commandPart) for commandPart in command]
         command = " ".join(sanitizedCommandList)
 
-        proc = pexpect.popen_spawn.PopenSpawn('bash', ['-c', "{} {} {}".format(self.binary, quote(command) + ("2>/dev/null" if hideErrors else ""))])
+        proc = pexpect.popen_spawn.PopenSpawn("bash -c {} {} {}".format(self.binary, quote(command), ("2>/dev/null" if hideErrors else "")))
         return self._process_proc_output(proc, command, printOnSuccess, hideErrors, prefillInput)
 
     def _process_proc_output(self, proc, command, printOnSuccess=False, hideErrors=False, prefillInput=''):
