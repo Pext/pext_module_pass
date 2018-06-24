087a8f0ef2d8d64fde120b6f3558647596a231b7
Sylvia van Os <sylvia@hackerchick.me>
2018-06-24 17:32:07 +0200

    Support for metadata i18n

--- compile_translations.sh
+++ compile_translations.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+pygettext2.6.py -d pext_module_pass -a -- *.py
+
 for dir in locale/*/LC_MESSAGES; do
+    msgmerge -U -N "$dir/pext_module_pass.po" pext_module_pass.pot
     msgfmt "$dir/pext_module_pass.po" -o "$dir/pext_module_pass.mo"
 done
+
+python3 generate_metadata.py
---
+++ generate_metadata.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2016 - 2018 Sylvia van Os <sylvia@hackerchick.me>
+#
+# Pext pass module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gettext
+import json
+import os
+
+languages = [dirname for dirname in os.listdir(os.path.join('.', 'locale')) if os.path.isdir(os.path.join('.', 'locale', dirname))] + [None]
+
+for language in languages:
+    if not language:
+        lang = gettext.NullTranslations()
+    else:
+        try:
+            lang = gettext.translation('pext_module_pass', localedir=os.path.join('.', 'locale'), languages=[language])
+        except FileNotFoundError:
+            lang = gettext.NullTranslations()
+            print("No {} metadata translation available for pext_module_pass".format(language))
+            continue
+
+    lang.install()
+    
+    filename = 'metadata_{}.json'.format(language) if language else 'metadata.json'
+    metadata_file = open(filename, 'w')
+    json.dump({'id': 'pext.module.pass',
+               'name': _('Password Store'),
+               'developer': 'Sylvia van Os',
+               'description': _('Allows Pext to do password management through pass'),
+               'homepage': 'https://pext.hackerchick.me/',
+               'license': 'GPL-3.0+',
+               'git_urls': ['https://github.com/Pext/pext_module_pass'],
+               'settings': [{'name': 'binary',
+                             'description': _('Binary to call for pass operations'),
+                             'defaults': 'pass'
+                            }, {
+                             'name': 'directory',
+                             'description': _('Password Store directory'),
+                             'default': '~/.password-store/'
+                            }],
+               'platforms': ['Linux', 'Darwin', 'Windows']},
+              metadata_file, indent=2, sort_keys=True)
